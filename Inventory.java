88f295f69a1b3946d4dd40366977970049399083
karthikprasadbsc24-beep <karthikprasadbsc24@rvu.edu.in>
2025-11-10 20:27:11 +0530

    Update Inventory.java

--- Inventory.java
+++ Inventory.java
@@ -1,14 +1,14 @@
 public class Inventory {
-    private Weapon gun;
+    private Weapon weapon;
     private Armor armor;
 
     public Inventory() {
-        this.gun = new Weapon("Fist", 0);
+        this.weapon = new Weapon("Fist", 0);
         this.armor = new Armor("Cloth", 0);
     }
 
-    public Weapon getGun() { return gun; }
-    public void setGun(Weapon gun) { this.gun = gun; }
+    public Weapon getWeapon() { return weapon; }
+    public void setWeapon(Weapon weapon) { this.weapon = weapon; }
 
     public Armor getArmor() { return armor; }
     public void setArmor(Armor armor) { this.armor = armor; }
