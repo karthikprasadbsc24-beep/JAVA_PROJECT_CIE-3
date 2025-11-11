ce82e2e5e3f23fc9fde123d48153aaa088271d06
chaitanyasbsc24-beep <chaitanyasbsc24@rvu.edu.in>
2025-11-11 07:12:47 +0530

    Update Armor.java

--- Armor.java
+++ Armor.java
@@ -2,11 +2,11 @@ public class Armor {
     private String name;
     private int block;
 
-    public Armor(String name, int block) {
+public Armor (String name,int block) {
         this.name = name;
         this.block = block;
     }
-
-    public String getName() { return name; }
-    public int getBlock() { return block; }
+public String getName() { return name;}
+public String getblock() {return block;}
 }
+    
