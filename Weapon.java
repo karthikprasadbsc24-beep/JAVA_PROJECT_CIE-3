7c4cb1785c69afa87e2dea1ce76099abe6c0e0d7
chaitanyasbsc24-beep <chaitanyasbsc24@rvu.edu.in>
2025-11-11 06:50:17 +0530

    Update Weapon.java

--- Weapon.java
+++ Weapon.java
@@ -2,11 +2,12 @@ public class Weapon {
     private String name;
     private int damage;
 
-    public Weapon(String name, int damage) {
-        this.name = name;
-        this.damage = damage;
-    }
-
-    public String getName() { return name; }
-    public int getDamage() { return damage; }
+public weapon(String name,int damage) {
+    this.name = name;
+    this.damage = damage;
 }
+    public String getName() {return name;}
+    public int getDamage() {return damage;}
+}
+    
+
